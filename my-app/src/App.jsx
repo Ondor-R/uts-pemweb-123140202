008beaca63e47fbf2a48e42c34d4e4dda8ed7885
Ondor-R <rehanondor@gmail.com>
2025-10-31 14:57:31 +0700

    add code utk fetch API

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header.jsx';
 
 function App() {
+const [coins, setCoins] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
 
+//fetch data dari API
+const fetchData = useCallback(async () => {
+  setLoading(true);
+  setError(null);
+
+  const api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=idr&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+  try {
+    const response = await fetch(api_url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    setCoins(data);
+  } catch (error) {
+    setError(error.message);
+  } finally {
+    setLoading(false);
+  }
+}, []);
+
+useEffect(() => {
+  fetchData();
+}, [fetchData]);
 
 return (
     <div className="app-container">
