4a8b2951e0418c678018f72e43faf403c6aae1d7
Ondor-R <rehanondor@gmail.com>
2025-11-01 22:19:53 +0700

    fix a lil problem

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useCallback, useState, useMemo} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header.jsx';
+import Header from './components/Header.jsx';
 import DataTable from './components/DataTable.jsx';
 import FilterForm from './components/SearchForm.jsx';
 import DetailCard from './components/DetailCard.jsx';
